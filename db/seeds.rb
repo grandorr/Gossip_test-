3fba42edb8077f52185836d27cf0fb4418451016
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-08-09 00:36:55 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,8 @@ City.destroy_all
   city = City.create(name: "Nice")
 
 
-20.times do
-c = User.create(password: "noob",city: City.all.sample,first_name: Faker::Name.first_name , last_name: Faker::Name.last_name, description: Faker::Quote.yoda,email: Faker::Internet.email,age: rand(8..99))
+
+c = User.create(username: "noob",password: "noob",city: City.all.sample,first_name: Faker::Name.first_name , last_name: Faker::Name.last_name, description: Faker::Quote.yoda,email: Faker::Internet.email,age: rand(8..99))
 g = Gossip.create(title: Faker::Cosmere.shard,content:  Faker::Quote.yoda, user: c )
 g = Gossip.create(title: Faker::Cosmere.shard,content:  Faker::Quote.yoda, user: c )
 co = Comment.create(content: "Merci", gossip: g, user: c)
-
-end
